0671fb8826ca62964415d49d145867948ff33e64
Rekha Bhupatiraju <therekha@gmail.com>
2025-11-19 08:44:37 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { Portrait } from './Portrait'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-wrap justify-center items-center min-h-screen gap-4'>
+      <div className='w-fit flex flex-col gap-4'>
+        <div className='tile'>
+          <Portrait />
+        </div>
+        <div className='tile'>
+          <p>Rekha Bhupatiraju</p>
+          <p><a href="https://github.com/therekha">Github</a> <a href="https://in.linkedin.com/in/rekhabhupatiraju">LinkedIn</a></p>
+        </div>
+      </div>
+      <div className='w-fit flex flex-col gap-4'>
+        <div className='tile'>
+          <p>i got a job! which means i don't need a nice lookin site anymore</p>
+          <p>so im replacing my website with the in-progress version even though it's clearly in-progress</p>
+        </div>
+        <div className='tile'>
+          <p>boo</p>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Portrait.tsx
@@ -0,0 +1,7 @@
+import portrait from './assets/portraits/straight.jpg';
+
+export function Portrait() {
+    return <img src={portrait} alt="a drawing of me!" />;
+}
+
+export default Portrait;
