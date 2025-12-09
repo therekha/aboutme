10a139a7b75c5725dfd8754780c4cf17f5000558
Rekha Bhupatiraju <therekha@gmail.com>
2025-12-09 12:24:58 +0530

    initial setup for api-based fieldss

--- src/CoolStuff.tsx
+++ src/CoolStuff.tsx
@@ -1,8 +1,44 @@
+import {useEffect, useState } from "react";
+
 export function CoolStuff() {
+    type Article = {
+        title: string;
+        url: string;
+        source: string; 
+        description: string;
+    }
+
+    let [articles, setArticles] = useState<Article[]>([]);
+
+    function getLinks(): Article[] {
+        //will b api call later
+        return [{
+            title: "How confessions can keep language models honest",
+            url: "https://openai.com/index/how-confessions-can-keep-language-models-honest/?utm_source=tldrai",
+            source: "OpenAI Blog",
+            description: "By asking the model to provide a \"confession report\" with an independent reward model that prioritizes honesty, researchers are able to identify reward-hacking behavior that emerges when multiple goals are present."
+        }]
+    }
+
+    
+    useEffect(() => {
+        setArticles(getLinks());
+    }, []); 
+
   return (
     <div>
       <h1><strong>cool stuff</strong></h1>
-      <p>it's gonna be an API call</p>
+        {articles.map((article) => ( 
+            <div key={article.url} className="mb-4">
+                <h1>
+                    <a href={article.url} className="text-rosepink-500 hover:underline">
+                        {article.title}
+                    </a>
+                </h1>
+                <p><em>{article.source}</em></p>
+                <p>{article.description}</p>
+            </div>
+        ))}
     </div>
   );
 }
--- src/Currently.tsx
+++ src/Currently.tsx
@@ -1,9 +1,19 @@
+import {useEffect, useState } from "react";
+
 export function Currently () {
+    let [books, setBooks] = useState<string[]>([]);
+
+    //dummy data for now
+    useEffect(() => {
+        setBooks(["wuthering heights", "playing to win"]);
+    }, []);
+    
     return (
         <div>
           <h1><strong>currently reading:</strong></h1>
-          <p>wuthering heights</p>
-          <p>playing to win</p>
+          {books.map((book) => (
+            <p key={book}>{book}</p>
+          ))}   
         </div>
     );
 }
